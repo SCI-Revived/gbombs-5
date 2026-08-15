411b04ab12c3b408d023c2036488bd89d5accca8
PhilULSCI <60559527+PhilULSCI@users.noreply.github.com>
2026-08-15 15:42:34 -0700

    Verifying damage convars work

--- lua/entities/gb5_light_b_50lb.lua
+++ lua/entities/gb5_light_b_50lb.lua
@@ -34,11 +34,11 @@ ENT.UseRandomSounds                  =  false
 ENT.UseRandomModels                  =  false
 ENT.Timed                            =  false
 
-ENT.ExplosionDamage                  =  15
+ENT.ExplosionDamage                  =  20
 ENT.PhysForce                        =  1500
 ENT.ExplosionRadius                  =  100
 ENT.PlayerDamageScale                =  1
-ENT.PropDamageScale                  =  1
+ENT.PropDamageScale                  =  10
 ENT.SpecialRadius                    =  575
 ENT.MaxIgnitionTime                  =  0 
 ENT.Life                             =  20                                  
